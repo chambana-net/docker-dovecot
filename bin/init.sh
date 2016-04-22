8247b81b51dc691b3e4cad26226dc000891acb90
Josh King <jking@chambana.net>
2016-04-22 00:06:49 -0400

    Change from using URIs to hosts, as the slashes interfere with sed.

--- bin/init.sh
+++ bin/init.sh
@@ -3,14 +3,14 @@
 . /opt/chambana/lib/common.sh
 
 CHECK_BIN sed
-CHECK_VAR DOVECOT_LDAP_URIS
+CHECK_VAR DOVECOT_LDAP_HOSTS
 CHECK_VAR DOVECOT_LDAP_BASE
 CHECK_VAR DOVECOT_LDAP_AUTH_BIND_USERDN
 
 MSG "Configuring Dovecot LDAP settings..."
 
 sed -i -e "s/^auth_bind_userdn\ *=.*/auth_bind_userdn\ =\ ${DOVECOT_LDAP_AUTH_BIND_USERDN}/" \
-	-e "s/^uris\ *=.*/uris\ =\ ${DOVECOT_LDAP_URIS}/" \
+	-e "s/^hosts\ *=.*/hosts\ =\ ${DOVECOT_LDAP_HOSTS}/" \
 	-e "s/^base\ *=.*/base\ =\ ${DOVECOT_LDAP_BASE}/" \
 	/etc/dovecot/dovecot-ldap.conf.ext
 
